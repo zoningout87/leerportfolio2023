37da27e9296ae15cb3829031af8dc72c6afc9352
Femke <femke@zoning-out.nl>
2023-09-24 20:44:18 +0200

    alle files geupdated met betere namen en bij vele een dropdown menu gemaakt.

---
+++ leerportfolio/js/script.js
@@ -0,0 +1,31 @@
+const img = document.getElementById('carousel');
+const rightBtn = document.getElementById('right-btn');
+const leftBtn = document.getElementById('left-btn');
+
+
+
+img.src = pictures[10];
+let position = 0;
+
+const moveRight = () => {
+    if (position >= pictures.length - 1) {
+        position = 0
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position + 1];
+    position++;
+}
+
+const moveLeft = () => {
+    if (position < 1) {
+        position = pictures.length - 1;
+        img.src = pictures[position];
+        return;
+    }
+    img.src = pictures[position - 1];
+    position--;
+}
+
+rightBtn.addEventListener("click", moveRight);
+leftBtn.addEventListener("click", moveLeft);
